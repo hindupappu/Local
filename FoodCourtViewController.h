0c7355abd786a1e888818654c6eeec1414379715
hindupappu <hindupappu@users.noreply.github.com>
2014-03-17 15:47:26 +0530

    Create FoodCourtViewController.h

---
+++ FoodCourtViewController.h
@@ -0,0 +1,18 @@
+//
+//  FoodCourtViewController.h
+//  FoodCourt
+//
+//  Created by hindupappu on 3/14/14.
+//  Copyright (c) 2014 hindupappu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodCourtViewController : UIViewController
+{
+    IBOutlet UIButton *foodCourtButton;
+}
+-(IBAction)foodCourtButtonClicked:(id)sender;
+-(IBAction)needToBeImplemented:(id)sender;
+
+@end
